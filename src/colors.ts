01177251bdbe7cc46294d069161c85386aa54324
Ben Davis <bendavis381@gmail.com>
2018-07-17 11:57:45 -0400

    Add background color + change border color

--- src/colors.ts
+++ src/colors.ts
@@ -10,9 +10,11 @@ const Colors = {
   navy80: 'rgba(36, 34, 50, 0.8)',
   navy60: 'rgba(36, 34, 50, 0.6)',
   navy40: 'rgba(36, 34, 50, 0.4)',
+  navy20: 'rgba(36, 34, 50, 0.2)',
   navy10: 'rgba(36, 34, 50, 0.1)',
   navy5: 'rgba(36, 34, 50, 0.05)',
   navy3: 'rgba(36, 34, 50, 0.03)',
+  navy2: 'rgba(36, 34, 50, 0.02)',
   purple80: 'rgba(93, 117, 246, 0.8)',
   purple60: 'rgba(93, 117, 246, 0.6)',
   purple20: 'rgba(93, 117, 246, 0.2)',
@@ -26,12 +28,14 @@ const Colors = {
   text: '',
   textWhiteFaded: '',
   shadow: '',
+  background: '',
 };
 
 Colors.primary = Colors.purple;
-Colors.border = Colors.purple20;
+Colors.border = Colors.navy20;
 Colors.text = Colors.navy;
 Colors.textWhiteFaded = Colors.white60;
 Colors.shadow = Colors.navy10;
+Colors.background = Colors.navy2;
 
 export default Colors;
